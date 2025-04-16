a90e140e73329d96d5d3b7053b2dfa5c53dc2402
Kirill <Kirill@a.a>
2025-04-16 19:03:33 +0300

    добавили разрешение на использования камеры

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:khromov/features/account/presentation/account_screen.dart';
+import 'package:khromov/features/auth/presentation/auth_screen.dart';
+import 'package:khromov/features/chat/presentation/chat_screen.dart';
+import 'package:khromov/features/feed/presentation/feed_screen.dart';
+import 'package:khromov/features/home/presentation/home_screen.dart';
+import 'package:khromov/features/register/presentation/register_screen.dart';
+import 'package:khromov/features/services/presentation/services_screen.dart';
+import 'package:khromov/shared/etc/navigation_wrapper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(MainApp(prefs: prefs));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  final SharedPreferences prefs;
+
+  const MainApp({super.key, required this.prefs});
+
+  @override
+  State<MainApp> createState() => MainAppState();
+
+  static MainAppState? of(BuildContext context) {
+    return context.findAncestorStateOfType<MainAppState>();
+  }
+}
+
+class MainAppState extends State<MainApp> {
+  ThemeMode themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    loadTheme();
+  }
+
+  void loadTheme() {
+    final theme = widget.prefs.getString('theme') ?? 'system';
+    print(widget.prefs.getString('recipes'));
+    setState(() {
+      themeMode = ThemeMode.values.firstWhere(
+        (e) => e.toString() == 'ThemeMode.$theme',
+        orElse: () => ThemeMode.system,
+      );
+    });
+  }
+
+  void changeTheme(ThemeMode mode) {
+    setState(() {
+      themeMode = mode;
+    });
+    widget.prefs.setString('theme', mode.toString().split('.').last);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Курс Flutter Проект Тест',
+      initialRoute: "/",
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeMode,
+      routes: {
+        "/": (context) => const NavigationWrapper(),
+        "/home": (context) => const HomeScreen(),
+        "/feed": (context) => const FeedScreen(),
+        "/chat": (context) => const ChatScreen(),
+        "/auth": (context) => const AuthScreen(),
+        "/register": (context) => const RegisterScreen(),
+        "/services": (context) => const ServicesScreen(),
+        "/account": (context) => const AccountScreen(),
+      },
     );
   }
 }
